ab5e063731415f2cbf87785cadf7fbe6ddb5b884
Harryson <harrysonhu@gmail.com>
2020-02-27 20:39:17 -0500

    Implement color-alternating overlay text

--- src/components/Dog/Dog.css.js
+++ src/components/Dog/Dog.css.js
@@ -1,5 +1,7 @@
 import { css } from 'emotion';
 
+export const BRAND_COLORS = ['#8093FF', '#FF502C', '#FF9472', '#FF91FF'];
+
 export const dogWrapperStyles = css`
     border: 4px solid lightgrey;
     border-radius: 4px;
@@ -10,4 +12,17 @@ export const dogWrapperStyles = css`
 export const imageStyles = css`
     width: 100%;
     height: 100%;
+    position: relative;
+`;
+
+export const overlayTextWrapper = css`
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 450px;
+`;
+
+export const overlayTextStyles = indexPos => css`
+    color: ${BRAND_COLORS[indexPos]};
+    font-weight: bold;
 `;
--- src/components/Dog/Dog.jsx
+++ src/components/Dog/Dog.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { dogWrapperStyles, imageStyles } from './Dog.css';
+import {
+    BRAND_COLORS,
+    dogWrapperStyles,
+    imageStyles,
+    overlayTextWrapper,
+    overlayTextStyles
+} from './Dog.css';
 
-export const Dog = ({ image }) => {
-    return (
-        <div className={dogWrapperStyles}>
-            <img className={imageStyles} src={image} alt='Dog not found :(' />
-        </div>
-    );
+const message = [
+    'W',
+    'e',
+    ' ',
+    '❤️',
+    ' ',
+    'l',
+    'o',
+    'v',
+    'e',
+    ' ',
+    'o',
+    'u',
+    'r',
+    ' ',
+    'p',
+    'u',
+    'p',
+    's',
+    '!'
+];
+
+export const Dog = ({ image }) => (
+    <div className={dogWrapperStyles}>
+        <img className={imageStyles} src={image} alt='Dog not found :(' />
+        <span className={overlayTextWrapper}>{renderOverlayMessage()}</span>
+    </div>
+);
+
+const renderOverlayMessage = () => {
+    return message.map((char, index) => {
+        return (
+            <span className={overlayTextStyles(index % BRAND_COLORS.length)}>
+                {char}
+            </span>
+        );
+    });
 };
